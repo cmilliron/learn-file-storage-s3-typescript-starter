3243d6520b4951797b716c04b48a53d06bc575d1
Cody Milliron <codymilliron.dev@gmail.com>
2026-04-09 07:55:45 -0400

    Clean up code and remove thumbnail link routes and functions

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -4,38 +4,7 @@ import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-import { createDataLink, getThumbnailUrl } from "./assets";
-
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
+import { createDataLink } from "./assets";
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -84,16 +53,7 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const buffer = Buffer.from(fileData);
   const imageData = buffer.toString("base64");
 
-  const newThumbnail: Thumbnail = {
-    data: fileData,
-    mediaType: mediaType,
-  };
-
-  videoThumbnails.set(video.id, newThumbnail);
-
-  // const thumbnailURL = getThumbnailUrl(cfg, videoId);
-
-  const thumbnailData = createDataLink(newThumbnail.mediaType, imageData);
+  const thumbnailData = createDataLink(mediaType, imageData);
 
   // Update Video in DB
   video.thumbnailURL = thumbnailData;
